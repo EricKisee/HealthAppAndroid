e0fca66321ee6b98a8ba75a0979b474f5cdecdf4
EricKisee <erickiseemulwa@gmail.com>
2023-09-05 14:19:37 +0300

    Polishing Login and Register activities

--- app/src/main/java/com/erickisee/healthapp/RegisterActivity.java
+++ app/src/main/java/com/erickisee/healthapp/RegisterActivity.java
@@ -2,13 +2,52 @@ package com.erickisee.healthapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    EditText edFirstName, edLastName, edEmail, edUsername, edPassword;
+    Button btnRegister;
+    TextView tvLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        init();
+    }
+
+    private void init () {
+        edFirstName = findViewById(R.id.editTextFirstName);
+        edLastName = findViewById(R.id.editTextLastName);
+        edEmail = findViewById(R.id.editTextEmail);
+        edUsername = findViewById(R.id.editTextUsername);
+        edPassword = findViewById(R.id.editTextPassword);
+        tvLogin = findViewById(R.id.textViewLogin);
+        btnRegister = findViewById(R.id.buttonRegister);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String firstName, lastName, email, username, password;
+                firstName = edFirstName.getText().toString();
+                lastName = edLastName.getText().toString();
+                email = edEmail.getText().toString();
+                username = edUsername.getText().toString();
+                password = edPassword.getText().toString();
+
+                if(firstName.length()==0 ||lastName.length()==0 ||email.length()==0 ||username.length()==0 ||password.length()==0){
+
+                }else{
+                    startActivity(new Intent(RegisterActivity.this, MainActivity.class));
+                }
+
+            }
+        });
     }
 }
